2058f1a085d82df7878902909998dc6de98cb891
8c6t <nkw8620@gmail.com>
2019-11-04 02:18:46 +0900

    7.2 MappedSuperclass - 매핑 정보 상속

--- ch07/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ch07/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
-public class Member {
+public class Member extends BaseEntity {
 
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
@@ -20,10 +20,6 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
-
-//    @ManyToMany
-//    @JoinTable(name = "MEMBER_PRODUCT")
-//    private List<Product> products = new ArrayList<>();
 
     @OneToMany(mappedBy = "member")
     private List<MemberProduct> memberProducts;
