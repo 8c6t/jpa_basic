1499b54f81110fb9f2f2d485af763d6794a2fd58
8c6t <nkw8620@gmail.com>
2019-11-05 10:06:52 +0900

    8.2 즉시 로딩과 지연 로딩

--- ch08/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ch08/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,8 +1,10 @@
 package hellojpa;
 
-import org.hibernate.Hibernate;
-
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -15,54 +17,30 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member1 = new Member();
-            member1.setUsername("hello1");
-            em.persist(member1);
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Team team2 = new Team();
+            team2.setName("teamB");
+            em.persist(team2);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeam(team);
+            em.persist(member);
 
             Member member2 = new Member();
-            member1.setUsername("hello2");
+            member2.setUsername("member1");
+            member2.setTeam(team2);
             em.persist(member2);
 
             em.flush();
             em.clear();
 
-//            Member findMember = em.find(Member.class, member.getId());
-//            Member findMember = em.getReference(Member.class, member.getId());
-//            System.out.println("before findMember = " + findMember.getClass());
-//            System.out.println("findMember.username = " + findMember.getUsername());
-//            System.out.println("after findMember = " + findMember.getClass());
-
-//            Member m1 = em.find(Member.class, member1.getId());
-////            Member m2 = em.find(Member.class, member2.getId());
-//            Member m2 = em.getReference(Member.class, member2.getId());
-//            System.out.println("m1: " + m1.getClass());
-////            logic(m1, m2);
-//
-//            Member reference = em.getReference(Member.class, member1.getId());
-//            System.out.println("Reference: " + reference.getClass());
-//
-//            System.out.println("a == a: " + (m1 == reference));
-
-//            Member refMember = em.getReference(Member.class, member1.getId());
-//            System.out.println("refMember = " + refMember.getClass()); // Proxy
-//
-//            Member findMember = em.find(Member.class, member1.getId());
-//            System.out.println("findMember = " + findMember.getClass()); //Member
-//
-//            System.out.println("refMember == findMember: " + (refMember == findMember));
-
-            Member refMember = em.getReference(Member.class, member1.getId());
-            System.out.println("refMember = " + refMember.getClass()); // Proxy
-            // refMember.getUsername();
-            Hibernate.initialize(refMember);
-
-            System.out.println("isLoaded: " + emf.getPersistenceUnitUtil().isLoaded(refMember));
-
-//            em.detach(refMember);
-//            em.close();
-            em.clear();
-
-            refMember.getUsername();
+            // fetch(em, member);
+            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class)
+                    .getResultList();
 
             tx.commit();
         } catch (Exception e) {
@@ -75,10 +53,13 @@ public class JpaMain {
         emf.close();
     }
 
-    private static void logic(Member m1, Member m2) {
-        // System.out.println("m1 == m2: " + (m1.getClass() == m2.getClass()) );
-        System.out.println("m1 == m2; " + (m1 instanceof Member));
-        System.out.println("m1 == m2; " + (m2 instanceof Member));
+    private static void fetch(EntityManager em, Member member) {
+        Member m = em.find(Member.class, member.getId());
+        System.out.println("m = " + m.getTeam().getClass());
+
+        System.out.println("=============================");
+        System.out.println("teamName = " + m.getTeam().getName());
+        System.out.println("=============================");
     }
 
 }
