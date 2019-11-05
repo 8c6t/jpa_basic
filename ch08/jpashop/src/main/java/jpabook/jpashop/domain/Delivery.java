46b01c3bdd548a7aea229ee739733e77a9a65f82
8c6t <nkw8620@gmail.com>
2019-11-05 23:12:04 +0900

    8.4 실전 예제 5 - 연관관계 정리

---
+++ ch08/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+public class Delivery extends BaseEntity {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
+
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
+    private Order order;
+
+}
