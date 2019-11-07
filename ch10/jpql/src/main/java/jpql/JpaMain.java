9f26ccf543db45b08a97b0070cd5a0897b676063
8c6t <nkw8620@gmail.com>
2019-11-07 15:07:13 +0900

    10.5 조인

--- ch10/jpql/src/main/java/jpql/JpaMain.java
+++ ch10/jpql/src/main/java/jpql/JpaMain.java
@@ -16,26 +16,35 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
 
-            for (int i = 0; i < 100; i++) {
-                Member member = new Member();
-                member.setUsername("member" + i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Member member = new Member();
+            member.setUsername("teamA");
+            member.setAge(10);
+
+            member.setTeam(team);
+
+            em.persist(member);
 
             em.flush();
             em.clear();
 
-            List<Member> result = em.createQuery("SELECT m FROM Member m ORDER BY m.age DESC", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
+            String inner = "SELECT m FROM Member m JOIN m.team t";
+            String outer = "SELECT m FROM Member m LEFT OUTER JOIN m.team t";
+            String theta = "SELECT m from Member m, Team t WHERE m.username = t.name";
+
+            // on
+            String joinOn = "SELECT m FROM Member m JOIN m.team t ON t.name = 'teamA'";
+            String outerOn = "SELECT m FROM Member m LEFT JOIN Team t ON m.username = t.name";
+
+            List<Member> result = em.createQuery(outerOn, Member.class)
                     .getResultList();
 
-            System.out.println("result.size = " + result.size());
-            for (Member member1 : result) {
-                System.out.println("member1 = " + member1);
-            }
+            System.out.println("result = " + result.size());
+
+
 
             tx.commit();
         } catch (Exception e) {
